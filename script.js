7409410699eb7ced5d89ddc4d5f6d51f68c13c70
lkucharzyk <lukaszkucharzyk17@gmail.com>
2023-05-09 17:13:08 +0200

    added color change & download image  option

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+
+ let clockStyle ={}
+
+  if (localStorage.getItem('style') !== null){
+    clockStyle = JSON.parse(localStorage.getItem('style'))
+  }else{
+    clockStyle = {
+      faceColor : '#c29976',
+      borderColor: '#800000',
+      markersColor: 'black',
+      lgHandsColor: '#800000',
+      secHandColor: 'white'
+    }
+  }
+  
+  const colorInputs = document.querySelectorAll('input');
+  colorInputs.forEach((input)=>input.addEventListener('change', changeStyle));
+
+  function changeStyle(e){
+    const input = e.target.getAttribute('id');
+    const value = e.target.value;
+
+    switch(input){
+      case 'face-color':
+        clockStyle.faceColor = value;
+      break;  
+      case 'border-color':
+        clockStyle.borderColor = value;
+      break;  
+      case 'line-color':
+        clockStyle.markersColor = value;
+      break;  
+      case 'large-hand-color':
+        clockStyle.lgHandsColor = value;
+      break;  
+      case 'second-hand-color':
+        clockStyle.secHandColor = value;
+      break;  
+    }
+    localStorage.setItem('style', JSON.stringify(clockStyle))
+    console.log(localStorage.getItem('style'));
+  }
+
+
 function cloak(){
   const now = new Date;
   const canvas = document.querySelector('canvas');
@@ -9,7 +53,7 @@ function cloak(){
   ctx.rotate(-Math.PI / 2); // rotate - 90deg
 
   ctx.strokeStyle = 'black';
-  ctx.fillStyle = '#c29976';
+  ctx.fillStyle = clockStyle.faceColor;
   ctx.lineWidth = 5;
   ctx.lineCap = 'round';
 
@@ -17,7 +61,7 @@ function cloak(){
   ctx.save();
   ctx.beginPath();
   ctx.lineWidth =14;
-  ctx.strokeStyle = '#800000';
+  ctx.strokeStyle = clockStyle.borderColor;
   ctx.arc(0, 0, 200, 0, Math.PI * 2);
   ctx.stroke();
   ctx.fill();
@@ -29,6 +73,7 @@ function cloak(){
     ctx.beginPath();
     deg = 360 / 12;
     ctx.rotate(deg * Math.PI / 180)
+    ctx.strokeStyle = clockStyle.markersColor;
     ctx.moveTo(150, 0);
     ctx.lineTo(170, 0);
     ctx.stroke();
@@ -42,6 +87,7 @@ function cloak(){
     }else{
       ctx.beginPath();
     deg = 360 / 60;
+    ctx.strokeStyle = clockStyle.markersColor;
     ctx.rotate(deg * Math.PI / 180)
     ctx.lineWidth = 2
     ctx.moveTo(150, 0);
@@ -64,7 +110,7 @@ function cloak(){
   ctx.moveTo(-5, 0);
   ctx.lineTo(90, 0);
   ctx.lineWidth = 10;
-  ctx.strokeStyle = '#800000';
+  ctx.strokeStyle = clockStyle.lgHandsColor;
   ctx.stroke();
   ctx.restore();
 
@@ -75,7 +121,7 @@ function cloak(){
   ctx.moveTo(-10, 0);
   ctx.lineTo(110, 0);
   ctx.lineWidth = 8;
-  ctx.strokeStyle = '#800000';
+  ctx.strokeStyle = clockStyle.lgHandsColor;
   ctx.stroke();
   ctx.restore();
 
@@ -86,7 +132,7 @@ function cloak(){
   ctx.moveTo(-15, 0);
   ctx.lineTo(130, 0);
   ctx.lineWidth = 6;
-  ctx.strokeStyle = 'white';
+  ctx.strokeStyle = clockStyle.secHandColor;
   ctx.stroke();
   ctx.restore();
 
@@ -95,3 +141,13 @@ function cloak(){
   requestAnimationFrame(cloak);
 }
 requestAnimationFrame(cloak);
+
+function saveAsImage(){
+  const link = document.createElement('a');
+  link.download = 'download.png';
+  link.href = document.querySelector('canvas').toDataURL();
+  link.click();
+  link.delete;
+}
+
+document.querySelector('#save-btn').addEventListener('click', saveAsImage)
